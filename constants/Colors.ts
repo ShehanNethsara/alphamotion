6fcc7d67b7618dc41ad477229cc24d67e915ec7c
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2026-01-28 11:37:30 +0530

    update colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,7 @@
-// export default {
-//   neon: '#CCFF00',
-//   black: '#000000',
-//   darkGray: '#1C1C1E',
-//   gray: '#505050',
-//   white: '#FFFFFF',
-// };
+export default {
+  neon: '#CCFF00',
+  black: '#000000',
+  darkGray: '#1C1C1E',
+  gray: '#505050',
+  white: '#FFFFFF',
+};
